70b6325238c8f86fc9f293e8778bc2d660480b6a
ogura <zhao.terryy@gmail.com>
2018-02-09 20:20:54 -0500

    initial commit

---
+++ main.lua
@@ -0,0 +1,27 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.mousepressed(x, y, button, istouch)
+
+end
+
+function love.mousereleased(x, y, button, istouch)
+
+end
+
+function love.keypressed(key)
+
+end
+
+function love.quit()
+
+end
